4e6f502abc44b0e5b9cd6e7b18ee3ce5f6342eec
Zeyneb Dzhelil <zizi_h@outlook.com>
2019-03-22 21:10:11 +0200

    Add comparison logic.

--- Monitoring/Monitor.cs
+++ Monitoring/Monitor.cs
@@ -13,18 +13,23 @@ using AForge.Video.DirectShow;
 using System.Drawing.Imaging;
 using System.Threading;
 using System.IO;
-
+using System.Diagnostics;
 
 namespace Monitoring
 {
    public partial class Monitor : Form
    {
+      private const int imageCompareReplacementCount = 10;
+      private const float similarityThreshold = 0.5f;
+      private const float compareLevel = 0.95f;
       private FilterInfoCollection videoDevices;
-      EuclideanColorFiltering filter = new EuclideanColorFiltering();
-      Color color = Color.Black;
-      GrayscaleBT709 grayscaleFilter = new GrayscaleBT709();
-      BlobCounter blobCounter = new BlobCounter();
-      int range = 120;
+      private EuclideanColorFiltering filter = new EuclideanColorFiltering();
+      private Color color = Color.Black;
+      private GrayscaleBT709 grayscaleFilter = new GrayscaleBT709();
+      private BlobCounter blobCounter = new BlobCounter();
+      private int range = 120;
+      private Bitmap bitmapCompare;
+      private int frameCounterSinceLastUpdate = 1;
 
       public Monitor()
       {
@@ -74,6 +79,52 @@ namespace Monitoring
          blobCounter.ProcessImage(grayImage);
 
          image = mImage;
+
+         frameCounterSinceLastUpdate++;
+         if (bitmapCompare == null || frameCounterSinceLastUpdate % imageCompareReplacementCount == 0)
+         {
+            Debug.WriteLine($"{Guid.NewGuid().ToString()} VALUES CHANGED");
+
+            bitmapCompare = grayImage.ToManagedImage();
+            return;
+         }
+
+         if (!CompareImages(bitmapCompare, grayImage.ToManagedImage()))
+         {
+            Debug.WriteLine($"{Guid.NewGuid().ToString()} THEY ARE DIFFERENT");
+         }
+         else
+         {
+            Debug.WriteLine($"{Guid.NewGuid().ToString()} THEY ARE SAME");
+         }
+      }
+
+      public static Boolean CompareImages(Bitmap imageOne, Bitmap imageTwo)
+      {
+         var newBitmap1 = ChangePixelFormat(new Bitmap(imageOne), System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+         var newBitmap2 = ChangePixelFormat(new Bitmap(imageTwo), System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+         // Setup the AForge library
+         var tm = new ExhaustiveTemplateMatching();
+
+         // Process the images
+         var results = tm.ProcessImage(newBitmap1, newBitmap2);
+
+         // Compare the results, 0 indicates no match so return false
+         if (results.Length <= 0)
+         {
+            return false;
+         }
+
+         // Return true if similarity score is equal or greater than the comparison level
+         var match = results[0].Similarity >= compareLevel;
+
+         return match;
+      }
+
+      private static Bitmap ChangePixelFormat(Bitmap inputImage, System.Drawing.Imaging.PixelFormat newFormat)
+      {
+         return (inputImage.Clone(new Rectangle(0, 0, inputImage.Width, inputImage.Height), newFormat));
       }
 
       private void button1_Click(object sender, EventArgs e)
